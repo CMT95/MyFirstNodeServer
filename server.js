d7de7e05091d40d6fd53a8a6f51d4fb71b46e210
Cihat Tokmak <cmt@Cihats-MacBook-Pro.local>
2017-09-08 10:21:21 +0200

    added index.html

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var http = require('http');
 
+
+
 var server = http.createServer(function(req, res){
+	
+	res.setHeader('Access-Control-Allow-Origin', '*');
+
 	res.writeHead(200,{'Content-type' : 'text/json'});
 	res.write('{"title" : "Hello JSON web API","text" : "text fra min API"}');
 	res.end();
